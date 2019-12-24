215ed14b6ea55f5123a5f691006be6fc1518635b
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2019-12-24 19:37:51 +0200

    Style home section

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+
+export const menuBtn = document.querySelector('.menu-btn');
+export const hamburger = document.querySelector('.menu-btn__burger');
+export const nav = document.querySelector('.nav');
+export const menuNav = document.querySelector('.menu-nav');
+export const navItems = document.querySelectorAll('.menu-nav__item');
+
